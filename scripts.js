26261267608d4dcfc9d45ca9044afeb26632e167
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-03 19:27:44 -0500

    completed part one

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
 "use strict";
 
-// Async function
-// Axios request -> query lucky number + ?json
 
 const $resultArea = $("#results");
 
+/**
+ *
+ * PART ONE
+ *
+ */
+//return the actual fact
 async function getNumberFact(num) {
   return await axios({
     method: "GET",
     url: `http://numbersapi.com/${num}?json`
   });
+}
+
+/**
+ *
+ * PART TWO
+ *
+ */
+//move responsibility of handling web response to where the request is made
+//assess name for promise variable
+async function putMultipleNumberFactsOnPage(...nums) {
+  let promise = await getMultipleNumberFacts(nums);
+  let numberFacts = Object.values(promise.data);
+  for (let fact of numberFacts) {
+    $resultArea.append(`<li>${fact}</li>`)
+  }
 }
 
 async function getMultipleNumberFacts(...nums) {
@@ -20,15 +39,52 @@ async function getMultipleNumberFacts(...nums) {
   });
 }
 
-async function putNumbersOnPage(...nums) {
-  let promise = await getMultipleNumberFacts(nums);
-  let numberFacts = Object.values(promise.data);
-  for (let fact of numberFacts) {
+
+// putMultipleNumberFactsOnPage(42, 23, 1);
+
+
+/**
+ *
+ * PART THREE
+ *
+ */
+//move any handling of promise.allsettled to function which makes request
+async function putSingleNumberFactsOnPage(num) {
+
+  let promise = await getSingleNumberMultipleFacts(num);
+
+  let facts = promise.map(promise => promise.value.data.text);
+  console.log(facts);
+  for (let fact of facts) {
     $resultArea.append(`<li>${fact}</li>`)
   }
 }
 
-putNumbersOnPage(42, 23, 1);
+
+async function getSingleNumberMultipleFacts(num){
+
+  let fact1 = axios({
+    method: "GET",
+    url: `http://numbersapi.com/${num}?json`
+  });
+
+  let fact2 = axios({
+    method: "GET",
+    url: `http://numbersapi.com/${num}?json`
+  });
+
+  let fact3 = axios({
+    method: "GET",
+    url: `http://numbersapi.com/${num}?json`
+  });
+
+  let fact4 = axios({
+    method: "GET",
+    url: `http://numbersapi.com/${num}?json`
+  });
+
+  return await Promise.allSettled([fact1, fact2, fact3, fact4]);
+}
 
 
-// console.log(Object.values(numberFacts))
+putSingleNumberFactsOnPage(17);
